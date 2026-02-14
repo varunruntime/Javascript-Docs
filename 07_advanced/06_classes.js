27f2b280ccc3bc7cc8cb7c03901b03dbabfada96
Varun Dev <varunscript404@gmail.com>
2026-02-15 01:56:44 +0530

    classes file updated

--- 07_advanced/06_classes.js
+++ 07_advanced/06_classes.js
@@ -2,11 +2,17 @@
 
 class Rectangle {
     constructor (width, height, color){ 
-// The constructor’s job is to initialize (set up) the object when it is created.        
+// The constructor’s job is to initialize (set up) the object when it is created.     
+// A special method inside the class that is called when you create an instance(an object) of the class
+// It's used to initialize the properties of the object      
         this.width = width
         this.height = height
         this.color = color
     }
+    static whoami(){
+        return "I am a rectangle"
+    }
+
     area (){
         const areaOfRectangle = this.width * this. height
         return areaOfRectangle
@@ -24,3 +30,7 @@ const rectangle1 = new Rectangle(10, 20, "black")
 console.log(rectangle1.area())
 console.log(rectangle1.perimeter())
 rectangle1.paint()
+console.log(Rectangle.whoami())
+
+// Static methods are not attached to the internal objects they are directly called or attached
+// through classes 
